ad26b62642ad74693dae043155a2812d83fc47f2
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-10-27 16:29:38 -0600

    fixed bad env.example file, some issues with the remote-deploy script are resolved

--- remote-deploy.sh
+++ remote-deploy.sh
@@ -20,6 +20,8 @@ echo "IP ADDR $IP"
 apt update;
 apt install docker-compose docker ufw certbot;
 
+ufw disable
+
 #configure firewall
 ufw default deny incoming
 ufw default allow outgoing
@@ -29,19 +31,19 @@ ufw allow 443/tcp
 mkdir -p certs
 
 # Comment if using LetsEncrypt
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/C=US/ST=UT/L=Orem/O=Altamir/CN=$DOMAIN" \
-    -keyout certs/certificate.key  -out certs/certificate.cert
-export SSL_CERT_LOCATION=/opt/certs/certificate.cert
-export SSL_KEY_LOCATION=/opt/certs/certificate.key
+# openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+#     -subj "/C=US/ST=UT/L=Orem/O=Altamir/CN=$DOMAIN" \
+#     -keyout certs/certificate.key  -out certs/certificate.cert
+# export SSL_CERT_LOCATION=/opt/certs/certificate.cert
+# export SSL_KEY_LOCATION=/opt/certs/certificate.key
 source ./.env
 export DOMAIN_NAME=$DOMAIN
 # Uncomment for LetsEncrypt certification
-# certbot certonly --standalone -n -d $DOMAIN --email $DOMAIN_CONTACT --agree-tos;
-# export SSL_CERT_LOCATION=/etc/letsencrypt/live/$DOMAIN/fullchain.pem
-# export SSL_KEY_LOCATION=/etc/letsencrypt/live/$DOMAIN/privkey.pem
-# mv /etc/
-# crontab -l | { cat; echo "@daily certbot renew --pre-hook \"docker-compose -f $LOCATION/docker-compose.yml down\" --post-hook \"docker-compose -f $LOCATION/docker-compose.yml up -d\""; } | crontab -
+certbot certonly --standalone -n -d $DOMAIN --email $DOMAIN_CONTACT --agree-tos;
+export SSL_CERT_LOCATION=/etc/letsencrypt/live/$DOMAIN/fullchain.pem
+export SSL_KEY_LOCATION=/etc/letsencrypt/live/$DOMAIN/privkey.pem
+
+crontab -l | { cat; echo "@daily certbot renew --pre-hook \"docker-compose -f $LOCATION/docker-compose.yml down\" --post-hook \"docker-compose -f $LOCATION/docker-compose.yml up -d\""; } | crontab -
 
 envsubst '\$DOMAIN_NAME \$SSL_CERT_LOCATION \$SSL_KEY_LOCATION \$APPLICATION_PORT' < nginx.default.conf > nginx.conf;
 cat nginx.conf;
